8042e61adc6a7a781e14e74e3b4fe7ff840d0ac7
opilane <opilane@HP-CZC3217070>
2024-08-26 16:01:44 +0300

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        [Key] // primaarvõti
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollment {  get; set; }
+
+    }
+}
